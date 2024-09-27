92e8f84d3d4b200cb9d1333d7515b23732f2a216
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2024-09-27 09:43:48 +0300

    viesti tulee chatboxiin

---
+++ public/scripts.js
@@ -0,0 +1,25 @@
+document.getElementById('chat-send-button').addEventListener('click', sendChatMessage);
+
+document.getElementById('chat-input').addEventListener('keypress', function (keyPressed) {
+  if (keyPressed.key === 'Enter') {
+    sendChatMessage();
+  }
+});
+
+
+function sendChatMessage() {
+    console.log('Viesti lähetetty');
+    const chatUserInput = document.getElementById('chat-input').value;
+    console.log(chatUserInput);
+    document.getElementById('chat-input').value = '';
+    addMessageToChatbox(chatUserInput);
+}
+
+function addMessageToChatbox(message) {
+    console.log('Viesti lisätty');
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    console.log(messageElement);
+    document.getElementById('chatbox').appendChild(messageElement);
+
+}
